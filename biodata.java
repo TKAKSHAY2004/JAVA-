61f24719abafc6b3bd237604a95f2188993d1062
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-12-14 09:36:03 +0530

    Create biodata.java

---
+++ biodata.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        String name= "AKSHAY";
+        System.out.println("Biodata\n");
+        System.out.println("Name:" + name);
+        System.out.println("DOB:" + 25 +"."+ 05 +"." + 2004);
+        System.out.println("Age:"+ 20);
+        System.out.println("Schoolings in MAHATMA MONTESSORI SCHOOL IN MADURAI");
+        System.out.println("UG in SRM Insitute Of Science and Technology");
+        
+    }
+}
