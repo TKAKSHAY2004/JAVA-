2c149e43e169acce29ca560a8766c2da6a41bc56
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-12-17 08:55:01 +0530

    Create combination of int and float.java

---
+++ combination of int and float.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Input {
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+    	
+        System.out.print("Enter an integer A: ");
+        int a = input.nextInt();
+        System.out.println("Enter an integer B: ");
+        Float b = input.nextFloat();
+        
+        Float sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // closing the scanner object
+        input.close();
+    }
+}
