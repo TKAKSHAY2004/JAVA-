aa99753494c9295de7924645711de668d21a12f0
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-12-19 11:53:34 +0530

    Create Adding elements in given array.java

---
+++ Adding elements in given array.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args){
+        int[] a = {1,2,3,4,5};
+        System.out.print("Elements in array A:\n");
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }
+        int[] b = {6,7,8,9,10};
+        System.out.print("\nElements in array B:\n");
+        for(int j=0; j<b.length;j++){
+            System.out.print(b[j] +  " ");
+        }
+        
+        int[] sum = new int[b.length];
+        for (int k = 0; k < sum.length; k++) {
+            sum[k] = a[k] + b[k];
+        }
+
+        System.out.print("\nSum of array A and B :\n");
+        for(int k=0;k<sum.length;k++){
+            System.out.print(sum[k] + " ");
+        }
+    }
+}
