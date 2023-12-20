a429a112a6be44f5e3a6139bf84c56c5395a3edc
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2023-12-20 09:22:49 +0530

    Update method functions using in addition of arrays.java

--- method functions using in addition of arrays.java
+++ method functions using in addition of arrays.java
@@ -31,3 +31,36 @@ public class Main{
         myMethod();
     }
 }
+
+
+
+
+/*output 
+Elements in array A:
+1 2 3 4 5 Elements in array B:
+6 7 8 9 10 
+Sum of array A and B :
+7 9 11 13 15 
+
+
+Elements in array A:
+1 2 3 4 5 
+Elements in array B:
+6 7 8 9 10 
+Sum of array A and B :
+7 9 11 13 15 
+
+
+Elements in array A:
+1 2 3 4 5 
+Elements in array B:
+6 7 8 9 10 
+Sum of array A and B :
+7 9 11 13 15 
+
+Elements in array A:
+1 2 3 4 5 
+Elements in array B:
+6 7 8 9 10 
+Sum of array A and B :
+7 9 11 13 15 */
