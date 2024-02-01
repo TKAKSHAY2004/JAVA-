72c0ac6844471843772bed977e1c560eafabfe22
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-02-01 09:37:38 +0530

    Create adding two given numbers.java

---
+++ adding two given numbers.java
@@ -0,0 +1,13 @@
+class Add{
+    public static void main(String[] args){
+        int a=6, b=5,sum;
+        System.out.println("A:" + a+"\n" + "B:" + b);
+        sum = a + b;
+        System.out.println("Sum:"+ sum);
+    }
+}
+
+
+//A:6
+//B:5
+//Sum:11
