125bf9c7ee2c09d307d71d00aed15a5cbcf66c69
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-02-15 00:28:49 +0530

    Create float value with certain decimal value.c

---
+++ float value with certain decimal value.c
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.Scanner; 
+public class Class211042010057 { 
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in); 
+        float var1 = input.nextFloat(); 
+        float var2 = input.nextFloat(); 
+        float ans=var1+var2 ; 
+        System.out.printf("%.3f",ans); 
+    } 
+}
+
+/*
+5.3
+2.6
+7.900 */
