3efaf7b3f11917f39179be5779b6b194b0ef40db
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-02-19 20:30:26 +0530

    Create with 2 decimals using string format.java

---
+++ with 2 decimals using string format.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+public class Class211042010057{
+	 public static void main(String[] args) { 
+	     Scanner input = new Scanner(System.in);
+	     int billwt;
+	     billwt = input.nextInt();
+	     System.out.println("The Tax is "+ (String.format("%.2f",billwt*(0.18f))));
+	     System.out.println("The Tip is "+ (String.format("%.2f",billwt*(0.05f))));
+	     System.out.println("Total Bill with Tax and Tip is "+ (String.format("%.2f",billwt + billwt*(0.18f) + billwt*(0.05f))));
+		
+	}
+}
