451dc50698809f85f47312295c3911879696bb18
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-02-22 19:33:49 +0530

    Create Division of 2 numbers.java

---
+++ Division of 2 numbers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+public class Class211042010057 {
+	 public static void main(String[] args) { 
+	     Scanner input = new Scanner(System.in);
+	     int km = input.nextInt();
+	     float lpd = input.nextFloat();
+	     System.out.printf("%.3f",km/lpd);
+	     System.out.println();
+		
+	}
+}
+
+/*
+245
+4.2
+58.333
+  */
