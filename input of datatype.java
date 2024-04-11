ad6fc7f11b4e3c14a4d234819ec7c855aa8e402a
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-04-11 09:16:48 +0530

    Create input of datatype.java

---
+++ input of datatype.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
+        System.out.println(a);
+        float b = sc.nextFloat();
+        System.out.println(b);
+        
+        /*String str1 = sc.next();
+         System.out.println(str1);
+         input:hello world
+         output: hello*/
+        
+        String str2 = sc.nextLine();
+        System.out.println(str2);
+        
+        char c = sc.next().charAt(0);
+        System.out.println(c);
+        
+        
+    }
+}
