1fc0616e3e37ed369909b887ec3b8364c2470b08
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-04-11 09:22:27 +0530

    Create printing character in the string.java

---
+++ printing character in the string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String c = sc.nextLine();
+        System.out.println(c);
+        System.out.println(c.charAt(5));
+        // to print multiple lines use triple couts """     """
+    }
+}
