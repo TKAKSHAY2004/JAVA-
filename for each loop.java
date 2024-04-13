3c87958ec230daee890d5b4e194a678a6c0eb0eb
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-04-13 09:19:12 +0530

    Create for each loop.java

---
+++ for each loop.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int[] arr = {12,45,23,56,76};
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
