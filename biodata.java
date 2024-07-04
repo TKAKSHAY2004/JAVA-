fdc4bb9dea6bc305783eff4ff5ef26f7c7c15926
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-07-04 11:29:21 +0530

    Update biodata.java

--- biodata.java
+++ biodata.java
@@ -10,3 +10,30 @@ public class main {
         
     }
 }
+
+
+/*
+import java.util.Scanner;
+class biodata{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter name:");
+        String str = input.nextLine();
+        
+        System.out.println("Enter age:");
+        int age = input.nextInt();
+        
+        System.out.println("Gender(M/F) :");
+        char gender = input.next().charAt(0);
+        
+        System.out.println("Enter contact number:");
+        long num = input.nextLong();
+        
+        System.out.println("Name:" + str);
+        System.out.println("Age:" + age);
+        System.out.println("Gender:" + gender);
+        System.out.println("Contact Number:" + num);
+    }
+}
+*/
