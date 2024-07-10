51a1d916e4fa8a2d6f7712da72b2b8e1421543c4
21CSS101J <113873680+TKAKSHAY2004@users.noreply.github.com>
2024-07-10 09:25:27 +0530

    Create If operation.java

---
+++ If operation.java
@@ -0,0 +1,12 @@
+class conditional {
+    public static void main(String[] args) {
+        boolean isAuthourized = true ;
+        
+        if(isAuthourized){
+            System.out.println("You have Logged in");
+        }
+        if(!isAuthourized){
+            System.out.println("You have not Logged in");
+        }
+    }
+}
